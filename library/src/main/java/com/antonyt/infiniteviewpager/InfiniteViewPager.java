c71978aed4a16d86acf70c89aa6715e289a1bdd4
Lee Joohyup <meruchi801@gmail.com>
2014-10-01 21:25:53 +0900

    smooth scroll 기능

--- library/src/main/java/com/antonyt/infiniteviewpager/InfiniteViewPager.java
+++ library/src/main/java/com/antonyt/infiniteviewpager/InfiniteViewPager.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
 
 /**
  * A {@link ViewPager} that allows pseudo-infinite paging with a wrap-around effect. Should be used with an {@link
@@ -14,10 +18,12 @@ public class InfiniteViewPager extends ViewPager {
 
     public InfiniteViewPager(Context context) {
         super(context);
+        setMyScroller();
     }
 
     public InfiniteViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setMyScroller();
     }
 
     @Override
@@ -56,7 +62,7 @@ public class InfiniteViewPager extends ViewPager {
     }
 
     public void setRealCurrentItem(int item) {
-        setRealCurrentItem(item, false);
+        setRealCurrentItem(item, true);
     }
 
     public void setRealCurrentItem(int item, boolean smoothScroll) {
@@ -75,4 +81,33 @@ public class InfiniteViewPager extends ViewPager {
             return 0;
         }
     }
+
+    private void setMyScroller()
+    {
+        try
+        {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new MyScroller(getContext()));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public class MyScroller extends Scroller
+    {
+        public MyScroller(Context context)
+        {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration)
+        {
+            super.startScroll(startX, startY, dx, dy, 300 /*1 secs*/);
+        }
+    }
+
 }
